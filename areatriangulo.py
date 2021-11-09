9f39ee8d497a61b0a549c39351d49de26d65ce82
humberto <hachevazquez10@gmail.com>
2021-11-09 16:09:01 -0600

    el resultado a calcular es el siguiente

--- areatriangulo.py
+++ areatriangulo.py
@@ -2,3 +2,5 @@ base=float(input("inserte el valor de la base"))
 altura=float(input("inserte el valor de la altura"))
 
 area= (base * altura)/2
+
+print("el resultado del triangulo es :" + str(area))
